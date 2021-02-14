d7ea6d61f7064196225ff6da440bed678443207e
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-02-14 19:43:56 -0400

    Múltiplos usuários

--- jogoteca.py
+++ jogoteca.py
@@ -12,6 +12,24 @@ class Jogo:
         self.console = console
 
 
+class Usuario:
+    def __init__(self, id, nome, senha):
+        self.id = id
+        self.nome = nome
+        self.senha = senha
+
+
+usuario1 = Usuario('caio', 'Caio Carvalho', 'hopes')
+usuario2 = Usuario('nico', 'Nico Steppat', '1234')
+usuario3 = Usuario('robin', 'Robin Scherbatsky', 'canada')
+
+usuarios = {
+    usuario1.id: usuario1,
+    usuario2.id: usuario2,
+    usuario3.id: usuario3
+}
+
+
 jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
 jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
 jogo3 = Jogo('Mortal Kombat', 'Luya', 'SNES')
@@ -48,11 +66,13 @@ def login():
 
 @app.route('/autenticar', methods=['POST', ])
 def autenticar():
-    if 'password' == request.form['senha']:
-        session['usuario_logado'] = request.form['usuario']
-        flash(request.form['usuario'] + ' logou com sucesso!')
-        proxima_pagina = request.form['proxima']
-        return redirect(proxima_pagina)
+    if request.form['usuario'] in usuarios:
+        usuario = usuarios[request.form['usuario']]
+        if usuario.senha == request.form['senha']:
+            session['usuario_logado'] = usuario.id
+            flash(usuario.nome + ' logou com sucesso!')
+            proxima_pagina = request.form['proxima']
+            return redirect(proxima_pagina)
     else:
         flash('Não logado, tente novamente!')
         return redirect(url_for('login'))
